deca6a70e42261bf1085bdaf37de47a5bc8c91c2
quincy2112 <52934221+quincy2112@users.noreply.github.com>
2026-08-12 19:42:12 -0500

    Route Debug.WriteLine to Console when running from PowerShell so debug logs appear; enable Debug.AutoFlush

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace DS4BatteryMapper
 {
@@ -8,6 +9,18 @@ namespace DS4BatteryMapper
         [STAThread]
         static void Main()
         {
+            // Ensure Debug.WriteLine goes to the console when running from PowerShell/Command Prompt
+            try
+            {
+                Debug.Listeners.Add(new TextWriterTraceListener(Console.Out));
+                Debug.AutoFlush = true;
+                Console.WriteLine("Debug listener attached: Debug.WriteLine will also appear in console output.");
+            }
+            catch (Exception)
+            {
+                // If attaching a console listener isn't possible, ignore and continue - Debug output will still go to attached debuggers
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
